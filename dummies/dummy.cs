4a859fe92680ba4557c7adbb09bbfc3ef8cc5903
Baerkanogue <baerkanogue@gmail.com>
2026-02-27 00:25:01 +0100

    fixes for C/C++, C#

--- dummies/dummy.cs
+++ dummies/dummy.cs
@@ -2,28 +2,31 @@
 using System;
 using System.Collections.Generic;
 
-class Person {
+class Person
+{
     public string Name { get; set; }
-    public Person(string n){ Name=n; }
+    public Person(string n) { Name = n; }
     public void Greet() => Console.WriteLine($"Hello {Name}");
 }
 
-class Program {
-    static int Add(int a,int b)=>a+b;
+class Program
+{
+    static int Add(int a, int b) => a + b;
 
-    static void Main() {
-        int x=5;
-        double y=3.14;
-        bool ok=true;
-        string s="Hi";
+    static void Main()
+    {
+        int x = 5;
+        double y = 3.14;
+        bool ok = true;
+        string s = "Hi";
 
-        var list = new List<int>{1,2,3};
+        var list = new List<int> { 1, 2, 3 };
         var p = new Person("Alice");
         p.Greet();
 
-        for(int i=0;i<2;i++)
-            Console.WriteLine(i + (i%2==0?" even":" odd"));
+        for (int i = 0; i < 2; i++)
+            Console.WriteLine(i + (i % 2 == 0 ? " even" : " odd"));
 
-        Console.WriteLine(Add(2,3));
+        Console.WriteLine(Add(2, 3));
     }
 }
