703e397ad3dab0912601988e11dd7a53224d33e6
Baerkanogue <baerkanogue@gmail.com>
2026-02-27 20:07:36 +0100

    errors

--- samples/sample.py
+++ samples/sample.py
@@ -1,27 +1,32 @@
 # omelette
 import math
 
-rate: float = 3.14             
+rate: float = 3.14
 rate_cos: float = math.cos(rate)
-name: str = "Alice"       
-flag: bool = True          
+name: str = "Alice"
+flag: bool = True
+
 
 def greet(who: str) -> str:
     return f"Hello {who}"
 
+
 class Person:
     species = "Human"
+
     def __init__(self, name: str):
         self.name: str = name
         self.age: int = 32
+
     def hello(self):
         print(greet(self.name))
 
+
 for i in range(2):
-    print(i, "even" if i%2==0 else "odd")
+    print(i, "even" if i % 2 == 0 else "odd")
 
 try:
-    1/0
+    1 / 0
 except ZeroDivisionError:
     pass
 finally:
